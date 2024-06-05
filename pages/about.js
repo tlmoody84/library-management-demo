6b67d7da50ee763740c5c3057c96a7d50b1a93c4
tlmoody84 <156528348+tlmoody84@users.noreply.github.com>
2024-06-04 22:41:09 -0400

    corrections made grammatically

--- pages/about.js
+++ pages/about.js
@@ -8,10 +8,10 @@ const About = () => {
             <Navigation />
             <div className="about">
                 <h2>About Me</h2>
-                <p>I have a [Bachelor's] degree in [Criminal Justice] from [Fayetteville State University] along with a [Full Stack Developer Certificate] from [CodeEx Academy].</p>
-                <p>My skills include [JavaScript, CSS, Bootstrap, HTML, React, and NodeJS].</p>
-                <p>I have [2] years of experience in [frontend and backend of web applications].</p>
-                <Image src="next/image img_1535" alt="My Photo" />  
+                <p>I have a Bachelor's degree in Criminal Justice from Fayetteville State University along with a Full Stack Developer Certificate from CodeEx Academy.</p>
+                <p>My skills include JavaScript, CSS, Bootstrap, HTML, React, and NodeJS.</p>
+                <p>I have 2 years of experience in frontend and backend of web applications.</p>
+                <Image src="next/image img_1535" alt="My Photo" width-55px height-55px/>  
             </div>
         </div>
     );
