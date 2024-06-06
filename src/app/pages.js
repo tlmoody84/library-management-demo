14c24180374243ac2250b08095ceed91266c54a7
tlmoody84 <156528348+tlmoody84@users.noreply.github.com>
2024-06-05 20:46:34 -0400

    adding code from class following

---
+++ src/app/pages.js
@@ -0,0 +1,21 @@
+// "use client";
+// import React from "react";
+// import { useState } from "react";
+
+
+// export default function ManagementPage() {
+//     const [count, setCount] = useState(0);
+
+//     return(
+
+//      <main className="min-h-screen m-0">
+//         <p>Count: {count} </p>
+//         <button 
+//         onClick={setCount(count + 1)}
+//         class="bg-emerald-500 hover:bg-emerald-700 text-white font-bold py-2 px-4 rounded">
+//             Button
+//         </button>
+//         </main>
+//     );
+// }
+
