2c7d98652a259fe840e412924e797a2b5addfc1d
tlmoody84 <156528348+tlmoody84@users.noreply.github.com>
2024-06-06 22:20:38 -0400

    errors

---
+++ page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <br></br>
+      <h1 style={{ fontSize: '3em', color: 'magenta' }}>Welcome to my Portfolio!</h1>
+      <br></br>
+      <br></br>
+      <p style={{ color: 'magenta' }}>
+      I'm a newly self-motivated web developer with a strong desire to create user-friendly and impactful applications. Let's explore some of my work!
+      </p>
+      <br></br>
+      <br></br>
+    </div>
+  );
+};
