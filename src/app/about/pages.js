4ccc97d6bfb1b7435579d630397dc6dfbda76815
tlmoody84 <156528348+tlmoody84@users.noreply.github.com>
2024-06-09 00:04:13 -0400

    trying to fix error

--- src/app/about/pages.js
+++ src/app/about/pages.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Image from 'next/image';
 
-const About = () => {
+const about = () => {
     return (
         <div>
             <navigation />
@@ -22,4 +22,4 @@ const About = () => {
     );
 };
 
-export default About;
+export default about;
